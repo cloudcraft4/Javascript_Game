7e75aa073d33a25c14a0764ffa44b7fbdbfaf077
CloudCraft4 <mgstaley@gmail.com>
2022-03-22 16:31:24 -0600

    Classmixin closer to being usuable with player

--- assets/classmixins.js
+++ assets/classmixins.js
@@ -1,5 +1,19 @@
 Game.classMixins = {};
 
+
+Game.classMixins.prototype.getOwner = function() {
+    return this._owner
+};
+
+Game.classMixins.prototype.rollHitDice = function() {
+    //This function is used whenever you have to roll hit dice
+    let entity = getOwner();
+    let HDsize = entity._hitDiceSize;
+    let hitDiceRoll = entity.rollDice(HDsize) + entity.getLevel();
+    return hitDiceRoll;
+};
+
+
 //Nothing here is tested!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // Mixins for classes
@@ -10,32 +24,43 @@ Game.classMixins.Fighter = {
         this._owner = entity;
             let constitution = entity.getConstitution();
             let constitutionModifer = entity.getModifer(constitution);
-        this._hitDice = entity.rollDice(10) + entity.getLevel();
+        this._hitDiceSize = 10;
         this._startingHP = 10 + constitutionModifer;
         this._levelingHP = entity.rollDice(10) + constitutionModifer;
         // this._startingGear = 
         // You are supposed to be able to choose starting gear at creation.  Not sure if I want to bother.
         // this._proficiencies = ["allArmor", "simpleWeapons", "martialWeapons"];
         // this._savingThrows = ['strength', 'constitution'];
-    getOwner: function() {
-        return this._owner
-        }
-    secondWind: function(user) {
+    },
+
+
+    //I am thinking that this here has the formula for how the abilities work.  However there will be a
+    //list somewhere that has the abilities that you actually have located on player I guess.
+    // I am not really sure how this works yet.
+
+    useSecondWind: function() {
         if (this._uses > 0) {
             var owner = getOwner();
             owner._hp += owner.rollDice(10) + owner.getLevel();
             this._uses -= 1;
+        } else {
+            console.log('You do not have any more uses of Second Wind');
         }
-    actionSurge: function(user) {
+    },
+    useActionSurge: function() {
         if (this._uses > 0) {
             // "Extra move that is at instance speed basically"
             // double your speed for a little bit maybe
             this._uses -= 1;
+        } else {
+            console.log('You do not have any more uses of Action Surge');
         }
-    extraAttack: function(user) {
+    },
+    // This might not even be needed because it is just an automatic thing that happens
+    extraAttack: function() {
         // Always attack twice
-        }
-    indomidable: function(user) {
+    },
+    indomidable: function() {
         if (this._uses > 0) {
             // reroll a saving throw that you fail...  not very good move for a computer.  Maybe add
             // percentage or something to saving rolls.
@@ -54,18 +79,15 @@ Game.classMixins.Wizard = {
         this._hitDice = entity.rollDice(6) + entity.getLevel();
         this._startingHP = 6 + constitutionModifer;
         this._levelingHP = entity.rollDice(6) + constitutionModifer;
-    getOwner: function() {
-        return this._owner
-        }
+    },
+
     spellMastery: function() {
     //
-    }
+    },
     signatureSpells: function() {
         //
-        }
+        },
     learnSpell: function() {
-        // 
-        }
+        //        
     },
 };
-
